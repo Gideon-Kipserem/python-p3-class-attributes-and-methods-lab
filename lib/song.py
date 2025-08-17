ad77787e2db6a688167a466a3bdc757510760dee
Gideon <gideonkimaiyo254@gmail.com>
2025-08-17 19:52:35 +0300

    Implement Song class with class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,58 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    artist_count = {}
+    genre_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Call class methods when a new song is created
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
+
+s1 = Song("99 Problems", "Jay-Z", "Rap")
+s2 = Song("Halo", "Beyonce", "Pop")
+s3 = Song("God’s Plan", "Drake", "Rap")
+
+print(Song.count)        
+print(Song.artists)       
+print(Song.genres)       
+print(Song.artist_count)  
+print(Song.genre_count)  
